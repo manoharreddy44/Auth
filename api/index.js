3a26daf8007bc190d0d68bc44a610b14a5d8a07a
Manohar <manoharreddy.koppala@gmail.com>
2024-12-23 11:13:55 +0530

    create database and connect

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+   console.log(error); 
+});
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log('Server is listening to port 3000');
 });
