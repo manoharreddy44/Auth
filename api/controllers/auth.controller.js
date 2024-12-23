031f9ba1883d849f96bfe8f0e720e9b60464339a
Manohar <manoharreddy.koppala@gmail.com>
2024-12-23 17:01:59 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js';
+
 
 
 export const signup = async (req, res, next) => {
@@ -12,4 +15,34 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({email : email});
+    if(!validUser){
+      return next(errorHandler(400, 'Invalid email or password'));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if(!validPassword){
+      return next(errorHandler(400, 'Invalid email or password'));
+    }
+
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+        expires : new Date(Date.now() + 3600000)
+      })
+      .status(200)
+      .json(rest);
+    
+
+
+  } catch (error) {
+    next(error);
+  }
+  
 };
