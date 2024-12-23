3dec156910e22d76e851cc7af3de3b56977ea3e6
Manohar <manoharreddy.koppala@gmail.com>
2024-12-23 14:27:54 +0530

    create middleware

--- api/index.js
+++ api/index.js
@@ -23,3 +23,13 @@ app.use('/api/auth', authRoutes);
 app.listen(3000, () => {
   console.log('Server is listening to port 3000');
 });
+
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  })
+});
